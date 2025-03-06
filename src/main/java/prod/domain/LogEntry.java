c9d64cacf2f1fb5c3c08f6b94afe4f8396eced64
basti <bastianroeling61@gmail.com>
2025-03-06 01:37:39 +0100

    initial

---
+++ src/main/java/prod/domain/LogEntry.java
@@ -0,0 +1,20 @@
+package prod.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "log")
+@Getter
+@Setter
+public class LogEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long logId;
+
+    private String typ;
+    private String detail;
+    private String datum;
+}
